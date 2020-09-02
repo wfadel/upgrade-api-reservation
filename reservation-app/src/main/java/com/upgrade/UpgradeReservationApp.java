02f9f44cd02b24764d8ae8c3a4aecce53e6ddd63
Waseem Fadel <wfadel@gmail.com>
2020-09-01 21:55:11 -0400

    Enabled the scheduler

--- reservation-app/src/main/java/com/upgrade/UpgradeReservationApp.java
+++ reservation-app/src/main/java/com/upgrade/UpgradeReservationApp.java
@@ -3,13 +3,15 @@ package com.upgrade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableScheduling
 public class UpgradeReservationApp {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         SpringApplication.run(UpgradeReservationApp.class, args);
     }
 }
